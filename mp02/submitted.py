49fad01607cbffc9601e8fd43fb31866df865494
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2023-02-01 16:20:06 -0600

    reformat

--- mp02/submitted.py
+++ mp02/submitted.py
@@ -9,16 +9,16 @@ function's docstring.
 import numpy as np
 from collections import Counter
 
-stopwords = set(["a","about","above","after","again","against","all","am","an","and","any","are","aren","'t","as","at","be","because","been","before","being","below","between","both","but","by","can","cannot","could","couldn","did","didn","do","does","doesn","doing","don","down","during","each","few","for","from","further","had","hadn","has","hasn","have","haven","having","he","he","'d","he","'ll","he","'s","her","here","here","hers","herself","him","himself","his","how","how","i","'m","'ve","if","in","into","is","isn","it","its","itself","let","'s","me","more","most","mustn","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan","she","she","'d","she","ll","she","should","shouldn","so","some","such","than","that","that","the","their","theirs","them","themselves","then","there","there","these","they","they","they","they","'re","they","this","those","through","to","too","under","until","up","very","was","wasn","we","we","we","we","we","'ve","were","weren","what","what","when","when","where","where","which","while","who","who","whom","why","why","with","won","would","wouldn","you","your","yours","yourself","yourselves"])
+stopwords = {"a","about","above","after","again","against","all","am","an","and","any","are","aren","'t","as","at","be","because","been","before","being","below","between","both","but","by","can","cannot","could","couldn","did","didn","do","does","doesn","doing","don","down","during","each","few","for","from","further","had","hadn","has","hasn","have","haven","having","he","he","'d","he","'ll","he","'s","her","here","here","hers","herself","him","himself","his","how","how","i","'m","'ve","if","in","into","is","isn","it","its","itself","let","'s","me","more","most","mustn","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan","she","she","'d","she","ll","she","should","shouldn","so","some","such","than","that","that","the","their","theirs","them","themselves","then","there","there","these","they","they","they","they","'re","they","this","those","through","to","too","under","until","up","very","was","wasn","we","we","we","we","we","'ve","were","weren","what","what","when","when","where","where","which","while","who","who","whom","why","why","with","won","would","wouldn","you","your","yours","yourself","yourselves"}
 
 def create_frequency_table(train):
     '''
     Parameters:
-    train (dict of list of lists) 
+    train (dict of list of lists)
         - train[y][i][k] = k'th token of i'th text of class y
 
     Output:
-    frequency (dict of Counters) 
+    frequency (dict of Counters)
         - frequency[y][x] = number of tokens of word x in texts of class y
     '''
     raise RuntimeError("You need to write this part!")
@@ -26,11 +26,11 @@ def create_frequency_table(train):
 def remove_stopwords(frequency):
     '''
     Parameters:
-    frequency (dict of Counters) 
+    frequency (dict of Counters)
         - frequency[y][x] = number of tokens of word x in texts of class y
 
     Output:
-    nonstop (dict of Counters) 
+    nonstop (dict of Counters)
         - nonstop[y][x] = frequency of word x in texts of class y,
           but only if x is not a stopword.
     '''
@@ -39,13 +39,13 @@ def remove_stopwords(frequency):
 def laplace_smoothing(nonstop, smoothness):
     '''
     Parameters:
-    nonstop (dict of Counters) 
+    nonstop (dict of Counters)
         - nonstop[y][x] = frequency of x in y, if x not a stopword
     smoothness (float)
         - smoothness = Laplace smoothing hyperparameter
 
     Output:
-    likelihood (dict of dicts) 
+    likelihood (dict of dicts)
         - likelihood[y][x] = Laplace-smoothed likelihood of x given y
         - likelihood[y]['OOV'] = likelihood of an out-of-vocabulary word given y
 
@@ -59,7 +59,7 @@ def naive_bayes(texts, likelihood, prior):
     Parameters:
     texts (list of lists) -
         - texts[i][k] = k'th token of i'th text
-    likelihood (dict of dicts) 
+    likelihood (dict of dicts)
         - likelihood[y][x] = Laplace-smoothed likelihood of x given y
     prior (float)
         - prior = the prior probability of the class called "pos"
@@ -77,7 +77,7 @@ def optimize_hyperparameters(texts, labels, nonstop, priors, smoothnesses):
         - texts[i][k] = k'th token of i'th text
     labels (list) - dev set labels
         - labels[i] = class label of i'th text
-    nonstop (dict of Counters) 
+    nonstop (dict of Counters)
         - nonstop[y][x] = frequency of word x in class y, x not stopword
     priors (list)
         - a list of different possible values of the prior
@@ -90,4 +90,3 @@ def optimize_hyperparameters(texts, labels, nonstop, priors, smoothnesses):
           m'th candidate prior and the n'th candidate smoothness
     '''
     raise RuntimeError("You need to write this part!")
-                          
