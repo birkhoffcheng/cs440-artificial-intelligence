aae1935eb8b472eb2911125cc3369d96de137c7b
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2023-02-01 19:56:20 -0600

    complete naive_bayes

--- mp02/submitted.py
+++ mp02/submitted.py
@@ -8,6 +8,7 @@ function's docstring.
 
 import numpy as np
 from collections import Counter
+import math
 
 stopwords = {"a","about","above","after","again","against","all","am","an","and","any","are","aren","'t","as","at","be","because","been","before","being","below","between","both","but","by","can","cannot","could","couldn","did","didn","do","does","doesn","doing","don","down","during","each","few","for","from","further","had","hadn","has","hasn","have","haven","having","he","he","'d","he","'ll","he","'s","her","here","here","hers","herself","him","himself","his","how","how","i","'m","'ve","if","in","into","is","isn","it","its","itself","let","'s","me","more","most","mustn","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan","she","she","'d","she","ll","she","should","shouldn","so","some","such","than","that","that","the","their","theirs","them","themselves","then","there","there","these","they","they","they","they","'re","they","this","those","through","to","too","under","until","up","very","was","wasn","we","we","we","we","we","'ve","were","weren","what","what","when","when","where","where","which","while","who","who","whom","why","why","with","won","would","wouldn","you","your","yours","yourself","yourselves"}
 
@@ -85,7 +86,26 @@ def naive_bayes(texts, likelihood, prior):
     hypotheses (list)
         - hypotheses[i] = class label for the i'th text
     '''
-    raise RuntimeError("You need to write this part!")
+    hypotheses = []
+    for text in texts:
+        max_class = None
+        max_prob = None
+        for cla in likelihood:
+            prob = 0
+            oov_likelihood = likelihood[cla]['OOV']
+            if cla == 'pos':
+                prob += math.log(prior)
+            else:
+                prob += math.log(1 - prior)
+            for w in text:
+                if w in stopwords:
+                    continue
+                prob += math.log(likelihood[cla].get(w, oov_likelihood))
+            if max_prob is None or prob > max_prob:
+                max_prob = prob
+                max_class = cla
+        hypotheses.append(max_class)
+    return hypotheses
 
 def optimize_hyperparameters(texts, labels, nonstop, priors, smoothnesses):
     '''
