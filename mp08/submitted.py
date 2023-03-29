a939aec8888c4b9f3dff8ba426b05ac0f6a75911
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2023-03-29 17:41:52 -0500

    init viterbi_ec

--- mp08/submitted.py
+++ mp08/submitted.py
@@ -131,4 +131,4 @@ def viterbi_ec(train, test):
 	output: list of sentences, each sentence is a list of (word,tag) pairs.
 			E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
 	'''
-	raise NotImplementedError("You need to write this part!")
+	return viterbi(train, test)
