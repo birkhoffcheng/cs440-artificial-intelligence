77eee0cba1d599adf3b0ee32988f8fec43b5547f
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2023-04-10 16:58:14 -0500

    retab

--- mp09/submitted.py
+++ mp09/submitted.py
@@ -23,217 +23,216 @@ from models import resnet18
 
 
 def unpickle(file):
-    import pickle
-    with open(file, 'rb') as fo:
-        dict = pickle.load(fo, encoding='bytes')
-    return dict
+	import pickle
+	with open(file, 'rb') as fo:
+		dict = pickle.load(fo, encoding='bytes')
+	return dict
 
 
 """
-1.  Define and build a PyTorch Dataset
+1. Define and build a PyTorch Dataset
 """
 class CIFAR10(Dataset):
-    def __init__(self, data_files, transform=None, target_transform=None):
-        """
-        Initialize your dataset here. Note that transform and target_transform
-        correspond to your data transformations for train and test respectively.
-        """
-        raise NotImplementedError("You need to write this part!")
+	def __init__(self, data_files, transform=None, target_transform=None):
+		"""
+		Initialize your dataset here. Note that transform and target_transform
+		correspond to your data transformations for train and test respectively.
+		"""
+		raise NotImplementedError("You need to write this part!")
 
-    def __len__(self):
-        """
-        Return the length of your dataset here.
-        """
-        raise NotImplementedError("You need to write this part!")
+	def __len__(self):
+		"""
+		Return the length of your dataset here.
+		"""
+		raise NotImplementedError("You need to write this part!")
 
-    def __getitem__(self, idx):
-        """
-        Obtain a sample from your dataset. 
+	def __getitem__(self, idx):
+		"""
+		Obtain a sample from your dataset.
 
-        Parameters:
-            x:      an integer, used to index into your data.
+		Parameters:
+			x:	an integer, used to index into your data.
 
-        Outputs:
-            y:      a tuple (image, label), although this is arbitrary so you can use whatever you would like.
-        """
-        raise NotImplementedError("You need to write this part!")
-    
+		Outputs:
+			y:	a tuple (image, label), although this is arbitrary so you can use whatever you would like.
+		"""
+		raise NotImplementedError("You need to write this part!")
+
 
 def get_preprocess_transform(mode):
-    """
-    Parameters:
-        mode:           "train" or "test" mode to obtain the corresponding transform
-    Outputs:
-        transform:      a torchvision transforms object e.g. transforms.Compose([...]) etc.
-    """
+	"""
+	Parameters:
+		mode:		"train" or "test" mode to obtain the corresponding transform
+	Outputs:
+		transform:	a torchvision transforms object e.g. transforms.Compose([...]) etc.
+	"""
 
-    raise NotImplementedError("You need to write this part!")
+	raise NotImplementedError("You need to write this part!")
 
 
 def build_dataset(data_files, transform=None):
-    """
-    Parameters:
-        data_files:      a list of strings e.g. "cifar10_batches/data_batch_1" corresponding to the CIFAR10 files to load data
-        transform:       the preprocessing transform to be used when loading a dataset sample
-    Outputs:
-        dataset:      a PyTorch dataset object to be used in training/testing
-    """
-    raise NotImplementedError("You need to write this part!")
+	"""
+	Parameters:
+		data_files:	a list of strings e.g. "cifar10_batches/data_batch_1" corresponding to the CIFAR10 files to load data
+		transform:	the preprocessing transform to be used when loading a dataset sample
+	Outputs:
+		dataset:	a PyTorch dataset object to be used in training/testing
+	"""
+	raise NotImplementedError("You need to write this part!")
 
 
 """
-2.  Build a PyTorch DataLoader
+2. Build a PyTorch DataLoader
 """
 def build_dataloader(dataset, loader_params):
-    """
-    Parameters:
-        dataset:         a PyTorch dataset to load data
-        loader_params:   a dict containing all the parameters for the loader. 
-        
-    Please ensure that loader_params contains the keys "batch_size" and "shuffle" corresponding to those 
-    respective parameters in the PyTorch DataLoader class. 
+	"""
+	Parameters:
+		dataset:		a PyTorch dataset to load data
+		loader_params:	a dict containing all the parameters for the loader.
 
-    Outputs:
-        dataloader:      a PyTorch dataloader object to be used in training/testing
-    """
-    raise NotImplementedError("You need to write this part!")
+	Please ensure that loader_params contains the keys "batch_size" and "shuffle" corresponding to those
+	respective parameters in the PyTorch DataLoader class.
+
+	Outputs:
+		dataloader:		a PyTorch dataloader object to be used in training/testing
+	"""
+	raise NotImplementedError("You need to write this part!")
 
 
 """
 3. (a) Build a neural network class.
 """
 class FinetuneNet(torch.nn.Module):
-    def __init__(self):
-        """
-        Initialize your neural network here. Remember that you will be performing finetuning
-        in this network so follow these steps:
-        
-        1. Initialize convolutional backbone with pretrained model parameters.
-        2. Freeze convolutional backbone.
-        3. Initialize linear layer(s). 
-        """
-        super().__init__()
-        ################# Your Code Starts Here #################
+	def __init__(self):
+		"""
+		Initialize your neural network here. Remember that you will be performing finetuning
+		in this network so follow these steps:
 
-        raise NotImplementedError("You need to write this part!")
-        ################## Your Code Ends here ##################
+		1. Initialize convolutional backbone with pretrained model parameters.
+		2. Freeze convolutional backbone.
+		3. Initialize linear layer(s).
+		"""
+		super().__init__()
+		################# Your Code Starts Here #################
 
-    def forward(self, x):
-        """
-        Perform a forward pass through your neural net.
+		raise NotImplementedError("You need to write this part!")
+		################## Your Code Ends here ##################
 
-        Parameters:
-            x:      an (N, input_size) tensor, where N is arbitrary.
+	def forward(self, x):
+		"""
+		Perform a forward pass through your neural net.
 
-        Outputs:
-            y:      an (N, output_size) tensor of output from the network
-        """
-        ################# Your Code Starts Here #################
+		Parameters:
+			x:	an (N, input_size) tensor, where N is arbitrary.
 
-        raise NotImplementedError("You need to write this part!")
-        ################## Your Code Ends here ##################
+		Outputs:
+			y:	an (N, output_size) tensor of output from the network
+		"""
+		################# Your Code Starts Here #################
+
+		raise NotImplementedError("You need to write this part!")
+		################## Your Code Ends here ##################
 
 
 """
-3. (b)  Build a model
+3. (b) Build a model
 """
 def build_model(trained=False):
-    """
-    Parameters:
-        trained:         a bool value specifying whether to use a model checkpoint
+	"""
+	Parameters:
+		trained:a bool value specifying whether to use a model checkpoint
 
-    Outputs:
-        model:           the model to be used for training/testing
-    """
-    net = FinetuneNet()
-    return net
+	Outputs:
+		model:		the model to be used for training/testing
+	"""
+	net = FinetuneNet()
+	return net
 
 
 """
-4.  Build a PyTorch optimizer
+4. Build a PyTorch optimizer
 """
 def build_optimizer(optim_type, model_params, hparams):
-    """
-    Parameters:
-        optim_type:      the optimizer type e.g. "Adam" or "SGD"
-        model_params:    the model parameters to be optimized
-        hparams:         the hyperparameters (dict type) for usage with learning rate 
+	"""
+	Parameters:
+		optim_type:		the optimizer type e.g. "Adam" or "SGD"
+		model_params:	the model parameters to be optimized
+		hparams:		the hyperparameters (dict type) for usage with learning rate
 
-    Outputs:
-        optimizer:       a PyTorch optimizer object to be used in training
-    """
-    raise NotImplementedError("You need to write this part!")
+	Outputs:
+		optimizer:		a PyTorch optimizer object to be used in training
+	"""
+	raise NotImplementedError("You need to write this part!")
 
 
 """
 5. Training loop for model
 """
 def train(train_dataloader, model, loss_fn, optimizer):
-    """
-    Train your neural network.
+	"""
+	Train your neural network.
 
-    Iterate over all the batches in dataloader:
-        1.  The model makes a prediction.
-        2.  Calculate the error in the prediction (loss).
-        3.  Zero the gradients of the optimizer.
-        4.  Perform backpropagation on the loss.
-        5.  Step the optimizer.
+	Iterate over all the batches in dataloader:
+		1. The model makes a prediction.
+		2. Calculate the error in the prediction (loss).
+		3. Zero the gradients of the optimizer.
+		4. Perform backpropagation on the loss.
+		5. Step the optimizer.
 
-    Parameters:
-        train_dataloader:   a dataloader for the training set and labels
-        model:              the model to be trained
-        loss_fn:            loss function
-        optimizer:          optimizer
-    """
+	Parameters:
+		train_dataloader:	a dataloader for the training set and labels
+		model:				the model to be trained
+		loss_fn:			loss function
+		optimizer:			optimizer
+	"""
 
-    ################# Your Code Starts Here #################
+	################# Your Code Starts Here #################
 
-    raise NotImplementedError("You need to write this part!")
-    ################## Your Code Ends here ##################
+	raise NotImplementedError("You need to write this part!")
+	################## Your Code Ends here ##################
 
 
 """
 6. Testing loop for model
 """
 def test(test_dataloader, model):
-    """
-    This part is optional.
+	"""
+	This part is optional.
 
-    You can write this part to monitor your model training process.
+	You can write this part to monitor your model training process.
 
-    Test your neural network.
-        1.  Make sure gradient tracking is off, since testing set should only
-            reflect the accuracy of your model and should not update your model.
-        2.  The model makes a prediction.
-        3.  Calculate the error in the prediction (loss).
-        4.  Print the loss.
+	Test your neural network.
+		1. Make sure gradient tracking is off, since testing set should only
+			reflect the accuracy of your model and should not update your model.
+		2. The model makes a prediction.
+		3. Calculate the error in the prediction (loss).
+		4. Print the loss.
 
-    Parameters:
-        test_dataloader:    a dataloader for the testing set and labels
-        model:              the model that you will use to make predictions
+	Parameters:
+		test_dataloader:	a dataloader for the testing set and labels
+		model:				the model that you will use to make predictions
 
 
-    Outputs:
-        test_acc:           the output test accuracy (0.0 <= acc <= 1.0)
-    """
+	Outputs:
+		test_acc:			the output test accuracy (0.0 <= acc <= 1.0)
+	"""
 
-    # test_loss = something
-    # print("Test loss:", test_loss)
-    raise NotImplementedError("You need to write this part!")
+	# test_loss = something
+	# print("Test loss:", test_loss)
+	raise NotImplementedError("You need to write this part!")
 
 """
 7. Full model training and testing
 """
 def run_model():
-    """
-    The autograder will call this function and measure the accuracy of the returned model.
-    Make sure you understand what this function does.
-    Do not modify the signature of this function (names and parameters).
+	"""
+	The autograder will call this function and measure the accuracy of the returned model.
+	Make sure you understand what this function does.
+	Do not modify the signature of this function (names and parameters).
 
-    Please run your full model training and testing within this function.
+	Please run your full model training and testing within this function.
 
-    Outputs:
-        model:              trained model
-    """
-    raise NotImplementedError("You need to write this part!")
-    
+	Outputs:
+		model:	trained model
+	"""
+	raise NotImplementedError("You need to write this part!")
