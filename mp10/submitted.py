66610872e763474f52c28a2752bfc7d237143e82
Birkhoff <23278129+birkhoffcheng@users.noreply.github.com>
2023-04-17 00:14:53 -0500

    implement compute_transition_matrix

--- mp10/submitted.py
+++ mp10/submitted.py
@@ -9,6 +9,11 @@ import numpy as np
 
 epsilon = 1e-3
 
+def is_wall(model, r, c):
+	if r < 0 or r >= model.M or c < 0 or c >= model.N:
+		return True
+	return model.W[r, c]
+
 def compute_transition_matrix(model):
 	'''
 	Parameters:
@@ -17,7 +22,71 @@ def compute_transition_matrix(model):
 	Output:
 	P - An M x N x 4 x M x N numpy array. P[r, c, a, r', c'] is the probability that the agent will move from cell (r, c) to (r', c') if it takes action a, where a is 0 (left), 1 (up), 2 (right), or 3 (down).
 	'''
-	raise RuntimeError("You need to write this part!")
+	M = model.M
+	N = model.N
+	P = np.zeros((M, N, 4, M, N))
+	for r in range(M):
+		for c in range(N):
+			if model.T[r, c]:
+				continue
+
+			# action = left
+			if is_wall(model, r, c - 1):
+				P[r, c, 0, r, c] += model.D[r, c, 0] # stays in place
+			else:
+				P[r, c, 0, r, c - 1] += model.D[r, c, 0] # goes left
+			if is_wall(model, r + 1, c):
+				P[r, c, 0, r, c] += model.D[r, c, 1] # stays in place
+			else:
+				P[r, c, 0, r + 1, c] += model.D[r, c, 1] # goes down
+			if is_wall(model, r - 1, c):
+				P[r, c, 0, r, c] += model.D[r, c, 2] # stays in place
+			else:
+				P[r, c, 0, r - 1, c] += model.D[r, c, 2] # goes up
+
+			# action = up
+			if is_wall(model, r - 1, c):
+				P[r, c, 1, r, c] += model.D[r, c, 0] # stays in place
+			else:
+				P[r, c, 1, r - 1, c] += model.D[r, c, 0] # goes up
+			if is_wall(model, r, c - 1):
+				P[r, c, 1, r, c] += model.D[r, c, 1] # stays in place
+			else:
+				P[r, c, 1, r, c - 1] += model.D[r, c, 1] # goes left
+			if is_wall(model, r, c + 1):
+				P[r, c, 1, r, c] += model.D[r, c, 2] # stays in place
+			else:
+				P[r, c, 1, r, c + 1] += model.D[r, c, 2] # goes right
+
+			# action = right
+			if is_wall(model, r, c + 1):
+				P[r, c, 2, r, c] += model.D[r, c, 0] # stays in place
+			else:
+				P[r, c, 2, r, c + 1] += model.D[r, c, 0] # goes right
+			if is_wall(model, r - 1, c):
+				P[r, c, 2, r, c] += model.D[r, c, 1] # stays in place
+			else:
+				P[r, c, 2, r - 1, c] += model.D[r, c, 1] # goes up
+			if is_wall(model, r + 1, c):
+				P[r, c, 2, r, c] += model.D[r, c, 2] # stays in place
+			else:
+				P[r, c, 2, r + 1, c] += model.D[r, c, 2] # goes down
+
+			# action = down
+			if is_wall(model, r + 1, c):
+				P[r, c, 3, r, c] += model.D[r, c, 0] # stays in place
+			else:
+				P[r, c, 3, r + 1, c] += model.D[r, c, 0] # goes down
+			if is_wall(model, r, c + 1):
+				P[r, c, 3, r, c] += model.D[r, c, 1] # stays in place
+			else:
+				P[r, c, 3, r, c + 1] += model.D[r, c, 1] # goes right
+			if is_wall(model, r, c - 1):
+				P[r, c, 3, r, c] += model.D[r, c, 2] # stays in place
+			else:
+				P[r, c, 3, r, c - 1] += model.D[r, c, 2] # goes left
+
+	return P
 
 def update_utility(model, P, U_current):
 	'''
